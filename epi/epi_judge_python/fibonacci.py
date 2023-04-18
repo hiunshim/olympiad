5ee6bcd2b05a910cf1db0d098f829c7589bf9b27
miffy <35790175+hiunshim@users.noreply.github.com>
2023-04-18 01:51:19 -0700

    fibonacci

--- epi/epi_judge_python/fibonacci.py
+++ epi/epi_judge_python/fibonacci.py
@@ -1,9 +1,15 @@
 from test_framework import generic_test
+from typing import Dict
 
+cache:  Dict[int, int] = {}
 
 def fibonacci(n: int) -> int:
     # TODO - you fill in here.
-    return -1
+    if n <= 1:
+      return n
+    elif n not in cache:
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return cache[n] 
 
 
 if __name__ == '__main__':
